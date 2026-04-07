869505f2dc4f5cf97ec64fe9e4d7a2a4f9b19117
Shreya-103 <shreyaalpha1@gmail.com>
2026-04-07 22:08:59 +0530

    made code clean

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,42 +2,31 @@ import React from "react";
 
 const Hero = () =>{
     return(
-        <div className="flex m-8">
+        <div className="flex m-8 sm:mr-0">
       {/* LEFT SIDE MAIN */}
       <div className="flex flex-col self-start w-1/2 sm:w-1/3 mt-4">
-        <h2 className="text-black text-2xl sm:text-6xl font-bold">Prospective</h2>
-        <h2 className="text-black text-2xl sm:text-6xl font-bold">customer</h2>
-        <h2 className="text-black text-2xl sm:text-6xl font-bold">Segmentation</h2>
+        <h2 className="text-black text-2xl sm:text-5xl font-bold">Prospective</h2>
+        <h2 className="text-black text-2xl sm:text-5xl font-bold">customer</h2>
+        <h2 className="text-black text-2xl sm:text-5xl font-bold">Segmentation</h2>
         <p className=" mt-4 text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum pariatur 
           voluptatibus suscipit odio aspernatur expedita explicabo a rerum esse officiis eum, totam, facilis
            provident repellat tenetur reprehenderit nam quae! Vel!</p>
       </div>
       {/* RIGHT SIDE MAIN */}
-      <div className="flex self-end w-1/2 ml-12 gap-5">
+        {/* <div className="w-full sm:w-1/2 ml-0 sm:ml-12 overflow-x-auto"> */}
+        <div className="w-full sm:w-1/2 ml-8 sm:ml-16 overflow-x-auto scroll-smooth">
+      <div className="flex gap-5">
       {/* <div className="flex-1 relative"> */}
-            <img src="./Screenshot (30).png" alt="imgI" className="h-120 w-65 border-transparent rounded-4xl object-cover relative"/>
-            {/* <div className="absolute inset-0 flex justify-around text-black p-4 items-end">
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur, rerum ut quae porro, neque sed 
-                dolore beatae consequatur ex laudantium ipsam quos fugit quisquam libero expedita laborum cupiditate debitis est!</p>
-            </div>
-        </div> */}
+            <img src="./Screenshot (30).png" alt="imgI" className="h-110 w-65 border-transparent rounded-4xl object-cover flex-shrink-0 relative"/>
+            
 
         {/* <div className="flex-2 relative"> */}
-            <img src="./Screenshot (69).png" alt="imgII" className="h-120 w-65 border-transparent rounded-4xl object-cover relative"/>
-            {/* <div className="absolute inset-0 flex justify-around text-black p-4 items-end">
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur, rerum ut quae porro, neque sed 
-                dolore beatae consequatur ex laudantium ipsam quos fugit quisquam libero expedita laborum cupiditate debitis est!</p>
-            </div>
-        </div> */}
-
+            <img src="./Screenshot (69).png" alt="imgII" className="h-110 w-65 border-transparent rounded-4xl object-cover flex-shrink-0 relative"/>
+            
         {/* <div className="flex-3 relative"> */}
-                <img src="./Screenshot (93).png" alt="imgIII" className="h-120 w-65 border-transparent rounded-4xl object-cover relative"/> 
-                {/* <div className="absolute inset-0 flex justify-around text-black p-4 items-end">
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur, rerum ut quae porro, neque sed 
-                    dolore beatae consequatur ex laudantium ipsam quos fugit quisquam libero expedita laborum cupiditate debitis est!</p>
-                </div> */}
-        {/* </div> */}
-
+                <img src="./Screenshot (93).png" alt="imgIII" className="h-110 w-65 border-transparent rounded-4xl object-cover flex-shrink-0"/> 
+               
+</div>
       </div>
 
     </div>
